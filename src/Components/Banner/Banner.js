50199bc998d6e37615d2ff28472adb79a716ef6d
rnimisha <nimisaraut.gmail.com>
2022-08-17 01:37:14 +0545

    banner animation added

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -7,32 +7,59 @@ import MiniInfo from '../MiniInfo/MiniInfo'
 import { motion } from "framer-motion"
 
 const Banner = () => {
-  return (
-    <div className='banner-container'>
-        <div className="banner-left">
-            <span>Providing you with </span>
-            <span>Education & Inspiration</span>
-            {/* <p>Discover best psrograms built with industry experts</p> */}
 
-            <button className='btn'>Explore courses</button>
-            <div className="aura-effect effect-pos1"></div>
-        </div>
+    const leftSideVariant = {
+        whileInView :{
+            x : [-100, 0],
+            transition: {
+                duration : 0.5,
+                ease: 'easeInOut'
+            }
+        }
+    }
+    const bottomVariant ={
+        whileInView :{
+            y : [100, 0],
+            transition: {
+                duration : 0.5,
+                ease: 'easeInOut'
+            }
+        }
+    }
+      
+    return (
+        <div className='banner-container'>
+            <div className="banner-left">
+                <motion.span variants={leftSideVariant} whileInView="whileInView">Providing you with </motion.span>
+                <motion.span variants={leftSideVariant} whileInView="whileInView">Education & Inspiration</motion.span>
+                {/* <p>Discover best psrograms built with industry experts</p> */}
 
-        <div className="banner-right">
-            <div className="banner-img-container">
-                <img src={mainimg} alt="mainbanner" />
-                <motion.div initial ={{opacity : 0}} animate ={{opacity : 1}} transition={{ delay: 1, duration : 1, type : 'tween'}} className="light-glow"></motion.div>
+                <motion.button variants={bottomVariant} whileInView="whileInView" className='btn'>Explore courses</motion.button>
+                <div className="aura-effect effect-pos1"></div>
             </div>
-            <div className="banner-mini-info">
-                <MiniInfo number={1290} text1={'Excelent'} text2={'courses'} id ={'1'}/>
-                <MiniInfo number={29} text1={'Proficient'} text2={'tutors'} id ={'0'}/>
-                <MiniInfo number={'100%'} text1={'Creative'} text2={'environment'} id ={'1'}/>
 
-                <span className="aura-effect effect-pos2"></span>
+            <div className="banner-right">
+                <div className="banner-img-container">
+                    <motion.img src={mainimg} alt="mainbanner" variants={bottomVariant} whileInView="whileInView"/>
+                    <motion.div 
+                    whileInView ={{scale : [0,1]}}
+                    transition = {{duration : 1 , delay : 1, ease : 'easeInOut'}}
+                    className="light-glow">
+                        
+                    </motion.div>
+
+                    {/* <motion.div initial ={{opacity : 0}} animate ={{opacity : 1}} transition={{ delay: 1, duration : 1, type : 'tween'}} className="light-glow"></motion.div> */}
+                </div>
+                <div className="banner-mini-info" >
+                    <MiniInfo number={1290} text1={'Excelent'} text2={'courses'} id ={'1'}/>
+                    <MiniInfo number={29} text1={'Proficient'} text2={'tutors'} id ={'0'}/>
+                    <MiniInfo number={'100%'} text1={'Creative'} text2={'environment'} id ={'1'}/>
+
+                    <span className="aura-effect effect-pos2"></span>
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Banner
--- src/Components/MiniInfo/MiniInfo.js
+++ src/Components/MiniInfo/MiniInfo.js
@@ -1,13 +1,27 @@
 import React from 'react'
 import './mini-info.css'
+import { motion } from "framer-motion"
+
 
 const MiniInfo = ({number, text1, text2, id}) => {
+
+  const rightSideVariant ={
+    whileInView :{
+        x : [200, 0],
+        transition: {
+            duration : 0.5,
+            ase: 'easeInOut'
+
+        }
+    }
+  }
+
   return (
-    <div className='mini-info-container' >
+    <motion.div className='mini-info-container' variants={rightSideVariant} whileInView="whileInView">
         <span style= {{color : id === '0' ? '#a0cb8a' : '#F7C744'}}>{number}</span>
         <span>{text1}</span>
         <span>{text2}</span>
-    </div>
+    </motion.div>
   )
 }
 
