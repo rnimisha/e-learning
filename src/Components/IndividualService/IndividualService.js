b7de1ef2b729133cd48c389c4fb0a86100906302
rnimisha <nimisaraut.gmail.com>
2022-08-17 15:55:28 +0545

    individual service component

---
+++ src/Components/IndividualService/IndividualService.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './individual-service.css'
+import service1 from '../../images/service1.webp'
+
+
+const IndividualService = () => {
+  return (
+    <div className='individual-container'>
+        <div className="indi-img">
+            <img src={service1} alt="" />
+        </div>
+        <div className="ind-title">
+            Scholarship
+        </div>
+        <p>
+        cfnncjmcfn
+        </p>
+    </div>
+  )
+}
+
+export default IndividualService
--- src/Components/Service/Service.js
+++ src/Components/Service/Service.js
@@ -2,16 +2,21 @@ import React from 'react'
 import './service.css'
 
 import serice1 from '../../images/service1.webp'
+import IndividualService from '../IndividualService/IndividualService'
 
 const Service = () => {
   return (
     <div className='service-container'>
         <div className="service-title">
-            <h1>Our Services</h1>
+            <p>Our Services</p>
+            <h1>What we offer?</h1>
         </div>
 
         <div className="service-details">
-            
+            <IndividualService/>
+            <IndividualService/>
+            <IndividualService/>
+            <IndividualService/>
         </div>
         
     </div>
