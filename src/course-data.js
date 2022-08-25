c6b96670a340cac086b1236f56bc777b4029cff9
rnimisha <nimisaraut.gmail.com>
2022-08-25 12:15:32 +0545

    course data created

---
+++ src/course-data.js
@@ -0,0 +1,51 @@
+const course =[
+    {
+        id : 1,
+        name : 'Acounting',
+        Trending : True,
+        Recommended : False,
+        img :'accounting.webp'
+    },
+    {
+        id : 2,
+        name : 'Economics',
+        Trending : False,
+        Recommended : True,
+        img :'economics.webp'
+    },
+    {
+        id : 3,
+        name : 'Biology',
+        Trending : True,
+        Recommended : False,
+        img :'medicine.webp'
+    },
+    {
+        id : 4,
+        name : 'Calculus',
+        Trending : False,
+        Recommended : True,
+        img :'calculus.webp'
+    },
+    {
+        id : 5,
+        name : 'Chemistry',
+        Trending : True,
+        Recommended : True,
+        img :'chemistry.webp'
+    },
+    {
+        id : 6,
+        name : 'Architecture',
+        Trending : False,
+        Recommended : True,
+        img :'architect.webp'
+    },
+    {
+        id : 7,
+        name : 'Website Devlopment',
+        Trending : True,
+        Recommended : True,
+        img :'webdev.webp'
+    },
+]
