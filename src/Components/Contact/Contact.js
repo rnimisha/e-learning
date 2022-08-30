30612cc859ce98afb0c9788006dc532a87084fcd
rnimisha <nimisaraut.gmail.com>
2022-08-30 16:41:57 +0545

    email sent useeffect

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import emailjs from '@emailjs/browser';
 import './contact.css'
 const Contact = () => {
+
+    const [isEmailsent, setEmailSent] = useState(false);
+
     const sendMail = (e)=>{
         e.preventDefault();
 
@@ -12,30 +15,47 @@ const Contact = () => {
             console.log(error.text);
         });
 
+        setEmailSent(true);
         e.target.reset();
     }
+
+    useEffect(() => {
+      if(isEmailsent === true)
+      {
+        setTimeout(()=>{
+            setEmailSent(false);
+        }, 5000)
+      }
+    
+    }, [isEmailsent])
+    
+
     return (
         <div className='contact-container'>
-                <div className="service-title">
-                    <p>Contact Us</p>
-                    <h1>Keep in touch</h1>
-                </div>
-                <div className="contact-form-container">
-                <form onSubmit={sendMail}>
-                    <div>
-                        <input type="text" placeholder='Name' name= 'name'/>
-                        <input type="text" placeholder='Email' name= 'email'/>
-                    </div>
-                    <div>
-                        <textarea name="message" placeholder='You message here...'></textarea>
-                    </div>
-                    <button className='btn' type='submit'>Send</button>
-                </form>
-                </div>
-                <div className="aura-effect effect-pos1"></div>
-                <div className="aura-effect effect-pos2"></div>
-
+            <div className="service-title">
+                <p>Contact Us</p>
+                <h1>Keep in touch</h1>
             </div>
+            <div className="contact-form-container">
+            {!isEmailsent && <form onSubmit={sendMail}>
+                <div>
+                    <input type="text" placeholder='Name' name= 'name'/>
+                    <input type="text" placeholder='Email' name= 'email'/>
+                </div>
+                <div>
+                    <textarea name="message" placeholder='You message here...'></textarea>
+                </div>
+                <button className='btn' type='submit'>Send</button>
+            </form>}
+            {isEmailsent &&
+            <p>success</p>
+
+            }
+            </div>
+            <div className="aura-effect effect-pos1"></div>
+            <div className="aura-effect effect-pos2"></div>
+
+        </div>
     )
 }
 
