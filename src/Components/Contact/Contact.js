a6fc4f189c5d04c5bdf174da2e901280b7696ef1
rnimisha <nimisaraut.gmail.com>
2022-08-30 16:27:50 +0545

    email js added

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,29 +1,42 @@
-import React from 'react'
+import React from 'react';
+import emailjs from '@emailjs/browser';
 import './contact.css'
 const Contact = () => {
-  return (
-    <div className='contact-container'>
-            <div className="service-title">
-                <p>Contact Us</p>
-                <h1>Keep in touch</h1>
-            </div>
-            <div className="contact-form-container">
-               <form action="">
-                <div>
-                    <input type="text" placeholder='Name'/>
-                    <input type="text" placeholder='Email'/>
-                </div>
-                <div>
-                    <textarea name="message" placeholder='You message here...'></textarea>
-                </div>
-                <button className='btn'>Send</button>
-               </form>
-            </div>
-            <div className="aura-effect effect-pos1"></div>
-            <div className="aura-effect effect-pos2"></div>
+    const sendMail = (e)=>{
+        e.preventDefault();
 
-        </div>
-  )
+        emailjs.sendForm('service_4iectkm', 'template_zqofwga', e.target, 'ol7dPKZzuzqQH69XF')
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
+
+        e.target.reset();
+    }
+    return (
+        <div className='contact-container'>
+                <div className="service-title">
+                    <p>Contact Us</p>
+                    <h1>Keep in touch</h1>
+                </div>
+                <div className="contact-form-container">
+                <form onSubmit={sendMail}>
+                    <div>
+                        <input type="text" placeholder='Name' name= 'name'/>
+                        <input type="text" placeholder='Email' name= 'email'/>
+                    </div>
+                    <div>
+                        <textarea name="message" placeholder='You message here...'></textarea>
+                    </div>
+                    <button className='btn' type='submit'>Send</button>
+                </form>
+                </div>
+                <div className="aura-effect effect-pos1"></div>
+                <div className="aura-effect effect-pos2"></div>
+
+            </div>
+    )
 }
 
 export default Contact
